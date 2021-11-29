690fc2add597f0e5744d5ffa7e5b616fc784bfc7
jyoti <jyotibansal9466@gmail.com>
2021-11-29 22:51:14 +0530

    React

---
+++ my-app/src/notes.js
@@ -0,0 +1,29 @@
+const notes=[
+    {
+        key:1,
+        title:"Try React",
+        content:"React has been designed from the start for gradual adoption, and you can use as little or as much React as you need." 
+
+    },
+    {
+         key:2,
+        title:"Add React to a Website",
+        content:"You can add React to an HTML page in one minute. You can then either gradually expand its presence, or keep it contained to a few dynamic widgets." 
+
+
+    },
+    {
+          key:3,
+        title:"Learn React",
+        content:"People come to React from different backgrounds and with different learning styles. Whether you prefer a more theoretical or a practical approach" 
+
+    },
+    {
+           key:4,
+        title:"JavaScript Resources",
+        content:"The React documentation assumes some familiarity with programming in the JavaScript language." 
+
+    },
+]
+
+export default notes;
